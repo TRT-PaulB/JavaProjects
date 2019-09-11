de1ad9b7dedd34c4f8fb84f7919754dc2900bcb7
TRT-PaulB <paul.bickell@raytheon.co.uk>
2019-09-11 14:42:51 +0100

    move app into jugtours project

--- RestDemo/src/main/java/com/greglturnquist/payroll/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.greglturnquist.payroll;
-
-import org.springframework.data.repository.CrudRepository;
-
-// JPA CRUD operations
-// https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#repositories.query-methods.details
-public interface EmployeeRepository extends CrudRepository<Employee, Long> {
-
-}
